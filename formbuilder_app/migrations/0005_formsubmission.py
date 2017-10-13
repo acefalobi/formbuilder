c878ba88bb57b42cf5d69b61a4c5643ee2f7837c
aceinteract <acefalobi@gmail.com>
2017-10-13 05:37:31 +0100

    Transfer

--- formbuilder_app/migrations/0005_formsubmission.py
+++ formbuilder_app/migrations/0005_formsubmission.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             name='FormSubmission',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('jsonSubmission', models.CharField(max_length=100000)),
+                ('jsonSubmission', models.TextField()),
                 ('publishedDate', models.DateTimeField(auto_now_add=True)),
                 ('form', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='formbuilder_app.Form')),
             ],
